b41e1d8a0eab714d7c0f5c1d8aaf6e5fbb63aa3e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-08-04 16:37:40 +0100

    More job

--- src/data/job.js
+++ src/data/job.js
@@ -10,129 +10,129 @@ export const job = {
     logoFit: "cover",
     logoPosition: "right",
     pages: [
-      // {
-      //   name: "page1",
-      //   elements: [
-      //     {
-      //       type: "panel",
-      //       name: "personal-info",
-      //       title: "Personal Information",
-      //       elements: [
-      //         {
-      //           type: "text",
-      //           name: "first-name",
-      //           title: "First name",
-      //           isRequired: true,
-      //         },
-      //         {
-      //           type: "text",
-      //           name: "last-name",
-      //           startWithNewLine: false,
-      //           title: "Last name",
-      //           isRequired: true,
-      //         },
-      //         {
-      //           type: "text",
-      //           name: "birthdate",
-      //           title: "Date of birth",
-      //           isRequired: true,
-      //           inputType: "date",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "panel",
-      //       name: "location",
-      //       title: "Your Location",
-      //       elements: [
-      //         {
-      //           type: "dropdown",
-      //           name: "country",
-      //           title: "Country",
-      //           choicesByUrl: {
-      //             url: "https://surveyjs.io/api/CountriesExample",
-      //           },
-      //         },
-      //         {
-      //           type: "text",
-      //           name: "city",
-      //           title: "City/Town",
-      //         },
-      //         {
-      //           type: "text",
-      //           name: "zip",
-      //           startWithNewLine: false,
-      //           title: "Zip code",
-      //           validators: [
-      //             {
-      //               type: "numeric",
-      //             },
-      //           ],
-      //           inputType: "number",
-      //         },
-      //         {
-      //           type: "text",
-      //           name: "address",
-      //           title: "Street address",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "email",
-      //       title: "Email",
-      //       inputType: "email",
-      //       placeholder: "mail@example.com",
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "salary",
-      //       title: "Expected salary (in US dollars)",
-      //       validators: [
-      //         {
-      //           type: "numeric",
-      //         },
-      //       ],
-      //       inputType: "number",
-      //     },
-      //     {
-      //       type: "dropdown",
-      //       name: "position",
-      //       title: "What position are you applying for?",
-      //       choices: [
-      //         {
-      //           value: "frontend",
-      //           text: "Frontend Developer",
-      //         },
-      //         {
-      //           value: "backend",
-      //           text: "Backend Developer",
-      //         },
-      //         {
-      //           value: "fullstack",
-      //           text: "Full-Stack Developer",
-      //         },
-      //         {
-      //           value: "intern",
-      //           text: "Intern",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       type: "text",
-      //       name: "start-date",
-      //       title: "Date available to start work",
-      //       isRequired: true,
-      //       inputType: "date",
-      //     },
-      //     {
-      //       type: "file",
-      //       name: "resume",
-      //       title: "Upload your resume",
-      //       acceptedTypes: "application/pdf",
-      //     },
-      //   ],
-      // },
+      {
+        name: "page1",
+        elements: [
+          {
+            type: "panel",
+            name: "personal-info",
+            title: "Personal Information",
+            elements: [
+              {
+                type: "text",
+                name: "first-name",
+                title: "First name",
+                isRequired: true,
+              },
+              {
+                type: "text",
+                name: "last-name",
+                startWithNewLine: false,
+                title: "Last name",
+                isRequired: true,
+              },
+              {
+                type: "text",
+                name: "birthdate",
+                title: "Date of birth",
+                isRequired: true,
+                inputType: "date",
+              },
+            ],
+          },
+          {
+            type: "panel",
+            name: "location",
+            title: "Your Location",
+            elements: [
+              {
+                type: "dropdown",
+                name: "country",
+                title: "Country",
+                choicesByUrl: {
+                  url: "https://surveyjs.io/api/CountriesExample",
+                },
+              },
+              {
+                type: "text",
+                name: "city",
+                title: "City/Town",
+              },
+              {
+                type: "text",
+                name: "zip",
+                startWithNewLine: false,
+                title: "Zip code",
+                validators: [
+                  {
+                    type: "numeric",
+                  },
+                ],
+                inputType: "number",
+              },
+              {
+                type: "text",
+                name: "address",
+                title: "Street address",
+              },
+            ],
+          },
+          {
+            type: "text",
+            name: "email",
+            title: "Email",
+            inputType: "email",
+            placeholder: "mail@example.com",
+          },
+          // {
+          //   type: "text",
+          //   name: "salary",
+          //   title: "Expected salary (in US dollars)",
+          //   validators: [
+          //     {
+          //       type: "numeric",
+          //     },
+          //   ],
+          //   inputType: "number",
+          // },
+          // {
+          //   type: "dropdown",
+          //   name: "position",
+          //   title: "What position are you applying for?",
+          //   choices: [
+          //     {
+          //       value: "frontend",
+          //       text: "Frontend Developer",
+          //     },
+          //     {
+          //       value: "backend",
+          //       text: "Backend Developer",
+          //     },
+          //     {
+          //       value: "fullstack",
+          //       text: "Full-Stack Developer",
+          //     },
+          //     {
+          //       value: "intern",
+          //       text: "Intern",
+          //     },
+          //   ],
+          // },
+          // {
+          //   type: "text",
+          //   name: "start-date",
+          //   title: "Date available to start work",
+          //   isRequired: true,
+          //   inputType: "date",
+          // },
+          // {
+          //   type: "file",
+          //   name: "resume",
+          //   title: "Upload your resume",
+          //   acceptedTypes: "application/pdf",
+          // },
+        ],
+      },
     ],
     showQuestionNumbers: "off",
     questionErrorLocation: "bottom",
